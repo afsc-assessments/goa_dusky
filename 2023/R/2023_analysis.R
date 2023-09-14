6d7e8909fba30b0b53d2c6a4dd56f4a5c6bbf07e
Ben <ben.williams@noaa.gov>
2023-09-14 15:00:58 -0800

    update a lot o'2023

--- 2023/R/2023_analysis.R
+++ 2023/R/2023_analysis.R
@@ -1,4 +1,6 @@
 # load ----
+# remotes::install_github("BenWilliams-NOAA/afscassess")
+# remotes::install_github("afsc-assessments/afscdata")
 # devtools::unload("afscassess")
 library(afscdata)
 library(afscassess)
@@ -11,10 +13,13 @@ TAC <- c(3676, 5389, 5372)
 
 
 # setup
-setup_folders(year)
+# setup_folders(year)
 accepted_model(2022, "m22.3a", year)
 
 # data ----
 goa_dusk(year, off_yr = TRUE)
 clean_catch(year, species=species, TAC=TAC)
 bts_biomass(year=year)
+
+proj_ak(year=year, last_full_assess = 2022, species="dusky", region="goa",
+        rec_age=4, folder="harvest_proj", off_yr=TRUE)
